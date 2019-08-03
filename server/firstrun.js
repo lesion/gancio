c4db2e6454bd1bbf8daed3d1ffb459a8e6d46752
lesion <lesion@autistici.org>
2019-08-03 15:46:42 +0200

    minor

--- server/firstrun.js
+++ server/firstrun.js
@@ -54,7 +54,7 @@ module.exports = {
     await settings.set('allow_anon_event', true)
     await settings.set('allow_recurrent_event', true)
     await settings.set('recurrent_event_visible', true)
-    await settings.set('allow_comments', true)
+    await settings.set('enable_federation', true)
 
     // add default notification
     consola.info('Add default notification')
