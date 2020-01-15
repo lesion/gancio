0ea0eba35cbeecf2f2d4eaa36d4c0486a392d444
les <lesion@autistici.org>
2020-01-15 23:31:28 +0100

    enable `about` mod from ui

--- pages/about.vue
+++ pages/about.vue
@@ -1,16 +1,24 @@
 <template lang="pug">
   el-main
-    h4 <nuxt-link to='/'><img src='/favicon.ico'/></nuxt-link> {{$t('common.info')}}
-
-    div(v-html='$t("about")')
-    hr
-    small Made with <a href='https://gancio.org'>Gancio {{settings.version}}</a>
-
+    .edit(v-if='$auth.user && $auth.user.is_admin')
+      Editor(v-if='$auth.user && $auth.user.is_admin'
+        v-model='about')
+      el-button.float-right(type='success' plain icon='el-icon-check'
+        @click='setSetting({key: "about", value: about})') {{$t('common.save')}}
+    div(v-else v-html='about')
 </template>
 <script>
-import { mapState } from 'vuex'
+import Editor from '@/components/Editor'
+import { mapState, mapActions } from 'vuex'
 export default {
+  components: { Editor },
+  data ({ $store }) {
+    return {
+      about: $store.state.settings.about || this.$t('about')
+    }
+  },
   computed: mapState(['settings']),
+  methods: mapActions(['setSetting']),
   head () {
     return {
       title: `${this.settings.title} - ${this.$t('common.info')}`
