474794e9ab318edf69583ca40fb4ab0e8c01df6d
les <lesion@autistici.org>
2020-01-15 23:47:17 +0100

    cleaning index

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,5 @@
 <template lang='pug'>
-  #home
-    Nav
-    Home
-
+  Home
 </template>
 <script>
 import Home from '~/components/Home.vue'
@@ -12,22 +9,13 @@ import { mapState } from 'vuex'
 
 export default {
   name: 'Index',
-  computed: mapState(['settings']),
-  async fetch ({ store, $axios }) {
-    try {
-      moment.tz.setDefault(store.state.settings.instance_timezone)
-      const now = new Date()
-      const events = await $axios.$get(`/event/${now.getMonth()}/${now.getFullYear()}`)
-      store.commit('setEvents', events)
-      const { tags, places } = await $axios.$get('/event/meta')
-      store.commit('update', { tags, places })
-    } catch (e) {
-      console.error(e)
-    }
+  components: { Nav, Home },
+  fetch ({ store }) {
+    moment.tz.setDefault(store.state.settings.instance_timezone)
   },
+  computed: mapState(['settings']),
   mounted (ctx) {
     moment.tz.setDefault(this.settings.instance_timezone)
-  },
-  components: { Nav, Home }
+  }
 }
 </script>
