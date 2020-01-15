6178b3c3016d21586fdf1395fb1e19f55acfb88e
les <lesion@autistici.org>
2020-01-15 23:29:51 +0100

    new modal layout

--- layouts/modal.vue
+++ layouts/modal.vue
@@ -1,12 +1,9 @@
 <template lang='pug'>
-  el-container
+  el-container#modal
     el-header
     el-row(:gutter='20')
       el-col(:span='12' :offset='6')
         el-card.mt-5
-          h4(slot='header') <img src='/favicon.ico'/> {{$route.name}}
+          h4(slot='header') <nuxt-link :to='"/"'><img src='/favicon.ico'/></nuxt-link> {{$route.name}}
           nuxt
 </template>
-<style lang="less">
-@import '../assets/style.less';
-</style>
