d444568e002bb57208206bc3b75825f5c2e8db16
les <lesion@autistici.org>
2020-01-15 23:27:11 +0100

    new default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,37 @@
-<template>
-  <nuxt />
+<template lang='pug'>
+  el-container#main(:class='{dark: $route.name==="index"}')
+    el-backtop
+    el-header#header
+      Nav
+    #content
+      nuxt
+    el-footer#footer
+      .row
+        .col.col-7
+          small <a href='https://gancio.org'>Gancio {{settings.version}}</a>
+        .col#links
+          nuxt-link(to='/about') about
+          a(href='https://gancio.org') docs
+          a(href='https://blog.gancio.org') blog
+          a(href='https://socialhub.activitypub.rocks/c/software/gancio/43') forum
+          a(href='https://framagit.org/les/gancio') source
+
 </template>
-<style lang="less">
-@import '../assets/style.less';
+<script>
+import Nav from '~/components/Nav.vue'
+import { mapState } from 'vuex'
+export default {
+  components: { Nav },
+  computed: mapState(['settings'])
+}
+</script>
+<style lang='less'>
+#footer {
+  a {
+    font-size: 1.1em;
+    &:hover {
+      border-bottom: 1px solid yellow;
+    }
+  }
+}
 </style>
