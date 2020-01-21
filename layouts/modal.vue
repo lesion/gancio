d1dbbebffb443c856fedfe1caaa25b01aba132be
les <lesion@autistici.org>
2020-01-21 01:18:45 +0100

    clean modal layout

--- layouts/modal.vue
+++ layouts/modal.vue
@@ -3,7 +3,5 @@
     el-header
     el-row(:gutter='20')
       el-col(:span='12' :offset='6')
-        el-card.mt-5
-          h4(slot='header') <nuxt-link :to='"/"'><img src='/favicon.ico'/></nuxt-link> {{$route.name}}
-          nuxt
+        nuxt
 </template>
