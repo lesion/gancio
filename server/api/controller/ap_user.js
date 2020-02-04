911a06dd186047f0f40549f85e92e4b0cc0671f7
les <lesion@autistici.org>
2020-02-04 23:40:35 +0100

    admin add block user from fedi

--- server/api/controller/ap_user.js
+++ server/api/controller/ap_user.js
@@ -2,10 +2,14 @@ const { ap_user: APUser } = require('../models')
 
 const apUserController = {
   async toggleBlock (req, res) {
-    const user_id = req.body.user_id
-    const user = await APUser.findByPk(user_id)
-    await user.update({ blocked: !user.blocked })
-    res.json(user)
+    const ap_id = req.body.ap_id
+    try {
+      const user = await APUser.findByPk(ap_id)
+      await user.update({ blocked: !user.blocked })
+      res.json(user)
+    } catch (e) {
+      res.sendStatus(404)
+    }
   }
 }
 
