26d293ed0f2f16ad0bf837c4418972626d909779
les <lesion@autistici.org>
2020-11-03 23:37:28 +0100

    minor

--- server/migrations/20201103161446-notification_queue.js
+++ server/migrations/20201103161446-notification_queue.js
@@ -5,13 +5,13 @@ module.exports = {
     return queryInterface.bulkInsert('notifications', [
 
       // send AP message
-      { action: 'Create', type: 'ap', filters: '{ is_visible: true }', createdAt: new Date(), updatedAt: new Date() },
-      { action: 'Update', type: 'ap', filters: '{ is_visible: true }', createdAt: new Date(), updatedAt: new Date() },
-      { action: 'Delete', type: 'ap', filters: '{ is_visible: true }', createdAt: new Date(), updatedAt: new Date() },
+      { action: 'Create', type: 'ap', filters: '{ "is_visible": true }', createdAt: new Date(), updatedAt: new Date() },
+      { action: 'Update', type: 'ap', filters: '{ "is_visible": true }', createdAt: new Date(), updatedAt: new Date() },
+      { action: 'Delete', type: 'ap', filters: '{ "is_visible": true }', createdAt: new Date(), updatedAt: new Date() },
 
       // send anon event to admin
-      { action: 'Create', type: 'admin_email', filters: '{ is_visible: false }', createdAt: new Date(), updatedAt: new Date() }
-    ]).catch(e => { })
+      { action: 'Create', type: 'admin_email', filters: '{ "is_visible": false }', createdAt: new Date(), updatedAt: new Date() }
+    ]).catch(e => { console.error(e) })
   },
 
   down: (queryInterface, Sequelize) => {
