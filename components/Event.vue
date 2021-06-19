25e004f035f958ee54a841ff2e7797df5def1bdb
les <lesion@autistici.org>
2021-06-19 12:21:25 +0200

    ops, fix #111

--- components/Event.vue
+++ components/Event.vue
@@ -1,6 +1,7 @@
 <template lang="pug">
   v-card.h-event.event.d-flex
     nuxt-link(:to='`/event/${event.slug || event.id}`')
+      v-img.u-featured.img(:src="`/media/thumb/${event.image_path || 'logo.svg' }`")
       v-icon.float-right.mr-1(v-if='event.parentId' color='success') mdi-repeat
       .title.p-name {{event.title}}
 
