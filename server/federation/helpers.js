37b966787bbbf4a6dd445c4932247e94c4341e84
les <lesion@autistici.org>
2021-06-25 11:43:50 +0200

    minor

--- server/federation/helpers.js
+++ server/federation/helpers.js
@@ -211,7 +211,10 @@ const Helpers = {
       log.info(`Actor ${req.body.actor} not found`)
       return res.status(401).send('Actor not found')
     }
-    if (httpSignature.verifySignature(parsed, user.object.publicKey.publicKeyPem)) { return next() }
+    if (httpSignature.verifySignature(parsed, user.object.publicKey.publicKeyPem)) {
+      log.debug(`Valid signature from ${req.body.actor} `)
+      return next()
+    }
 
     // still not valid
     log.info(`Invalid signature from user ${req.body.actor}`)
