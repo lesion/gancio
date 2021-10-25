83a5ebc5e15e307782bf5d56f2a0561093f6150e
lesion <lesion@autistici.org>
2021-10-25 13:17:49 +0200

    add i and span to allowed tags

--- server/helpers.js
+++ server/helpers.js
@@ -57,7 +57,7 @@ module.exports = {
 
   sanitizeHTML (html) {
     return domPurify.sanitize(html, {
-      ALLOWED_TAGS: ['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'br',
+      ALLOWED_TAGS: ['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'br', 'i', 'span',
         'h6', 'b', 'a', 'li', 'ul', 'ol', 'code', 'blockquote', 'u', 's', 'strong'],
       ALLOWED_ATTR: ['href', 'target']
     })
