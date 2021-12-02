d43cfbd6f94acdf389c02ca4a5d2c38833b23496
lesion <lesion@autistici.org>
2021-12-02 12:12:16 +0100

    add lang html attrs with current locale

--- layouts/default.vue
+++ layouts/default.vue
@@ -19,9 +19,16 @@ import Confirm from '../components/Confirm'
 import { mapState } from 'vuex'
 
 export default {
+  head () {
+    return {
+      htmlAttrs: {
+        lang: this.locale
+      }
+    }
+  },
   name: 'Default',
   components: { Nav, Snackbar, Footer, Confirm },
-  computed: mapState(['settings']),
+  computed: mapState(['settings', 'locale']),
   created () {
     this.$vuetify.theme.dark = this.settings['theme.is_dark']
   }
