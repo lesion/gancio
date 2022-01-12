a5f738fce222a48fb7eaaec9d2d0457e13e6aba7
lesion <lesion@autistici.org>
2022-01-12 21:44:26 +0100

    begin with multisite model and migration

---
+++ server/api/models/site.js
@@ -0,0 +1,48 @@
+const sequelize = require('./index').sequelize
+const { Model, DataTypes } = require('sequelize')
+
+class Site extends Model {}
+
+Site.init({
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: DataTypes.INTEGER
+  },  
+  hostname: {
+    type: DataTypes.STRING,
+    unique: true
+  },
+  is_active: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: true
+  }
+  // name: DataTypes.STRING,
+  // description: DataTypes.STRING,
+  // blocked: DataTypes.BOOLEAN,
+}, { sequelize, modelName: 'sites' })
+
+const Place = require('./place')
+// const Resource = require('./resource')
+const Setting = require('./setting')
+const Tag = require('./tag')
+const User = require('./user')
+const Announcement = require('./announcement')
+
+Site.hasMany(Place)
+Place.belongsTo(Site)
+
+Site.hasMany(Setting)
+Setting.belongsTo(Site)
+
+Site.hasMany(Tag)
+Tag.belongsTo(Site)
+
+Site.hasMany(User)
+User.belongsTo(Site)
+
+Site.hasMany(Announcement)
+Announcement.belongsTo(Site)
+
+module.exports = Site
---
+++ server/migrations/20211225215943-create-multisite.js
@@ -0,0 +1,160 @@
+'use strict';
+
+const { query } = require("../log");
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    const transaction = await queryInterface.sequelize.transaction()
+    try {
+      await queryInterface.createTable('sites', {
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: Sequelize.INTEGER
+        },
+        hostname: {
+          type: Sequelize.STRING,
+          unique: true
+        },
+        is_active: {
+          type: Sequelize.BOOLEAN,
+          defaultValue: true
+        },
+        createdAt: {
+          allowNull: false,
+          type: Sequelize.DATE
+        },
+        updatedAt: {
+          allowNull: false,
+          type: Sequelize.DATE
+        }
+      }, { transaction })
+      // await queryInterface.removeConstraint('settings', 'settings_pkey',{ transaction }); // myTable_pkey = name of constraint
+      await queryInterface.sequelize.query('ALTER TABLE settings RENAME TO old_settings', { transaction })
+
+      await queryInterface.createTable('settings', {
+        key: {
+          type: Sequelize.STRING,
+          primaryKey: true,
+          allowNull: false,
+          index: true
+        },
+        siteId: {
+          type: Sequelize.INTEGER,
+          primaryKey: true,
+          allowNull: true,
+          index: true,
+          defaultValue: null,
+          references: {
+            model: 'sites',
+            key: 'id'
+          },
+          onUpdate: 'CASCADE',
+          onDelete: 'CASCADE'          
+        },        
+        value: Sequelize.JSON,
+        is_secret: Sequelize.BOOLEAN,
+        createdAt: {
+          allowNull: false,
+          type: Sequelize.DATE
+        },
+        updatedAt: {
+          allowNull: false,
+          type: Sequelize.DATE
+        }        
+      }, { transaction })
+
+      await queryInterface.sequelize.query('INSERT INTO settings SELECT key, NULL, value, is_secret, createdAt, updatedAt from old_settings', { transaction })
+      // await queryInterface.changeColumn('settings', 'key', {
+      //   type: Sequelize.STRING,
+      //   allowNull: false,
+      //   index: true,
+      //   unique: false,
+      //   primaryKey: false
+      // }, { transaction })
+      // await queryInterface.addColumn('settings', 'siteId', {
+      //   type: Sequelize.INTEGER,
+      //   references: {
+      //     model: 'sites',
+      //     key: 'id'
+      //   },
+      //   onUpdate: 'CASCADE',
+      //   onDelete: 'CASCADE'
+      // }, { transaction })
+      // await queryInterface.addConstraint('settings', {
+      //   type: 'primary key',
+      //   name: 'custom_primary',
+      //   fields: ['key', 'siteId']
+      // })
+      await queryInterface.addColumn('places', 'siteId', {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'sites',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      }, { transaction })
+      await queryInterface.addColumn('tags', 'siteId', {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'sites',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      }, { transaction })
+      await queryInterface.addColumn('events', 'siteId', {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'sites',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      }, { transaction })
+      await queryInterface.addColumn('users', 'siteId', {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'sites',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      }, { transaction })
+      await queryInterface.addColumn('announcements', 'siteId', {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'sites',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      }, { transaction })      
+      await transaction.commit()
+    } catch (err) {
+      console.error(err)
+      await transaction.rollback()
+      throw err
+    }
+  },
+  async down (queryInterface, Sequelize) {
+    const transaction = await queryInterface.sequelize.transaction()
+    try {
+      await queryInterface.dropTable('sites')
+      await queryInterface.removeColumn('settings', 'siteId')
+      await queryInterface.removeColumn('places', 'siteId')
+      await queryInterface.removeColumn('tags', 'siteId')
+      await queryInterface.removeColumn('events', 'siteId')
+      await queryInterface.removeColumn('users', 'siteId')
+      // await queryInterface.removeColumn('announcements', 'siteId')
+      await transaction.commit()
+    } catch (err) {
+      console.error(err)
+      await transaction.rollback()
+      throw err
+    }
+
+  }
+}
