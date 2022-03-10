c54706bfbc38a1624f23b16dcbeea5a3342713ea
lesion <lesion@autistici.org>
2022-03-10 12:34:53 +0100

    add an alert to specify user has to copy the admin password during setup

--- pages/setup/Completed.vue
+++ pages/setup/Completed.vue
@@ -2,17 +2,18 @@
   v-container
     v-card-title.d-block.text-h5.text-center(v-text="$t('setup.completed')")
     v-card-text(v-html="$t('setup.completed_description', user)")
+    v-alert.mb-3.mt-1(outlined type='warning' color='red' show-icon :icon='mdiAlert') {{$t('setup.copy_password_dialog')}}
     v-card-actions
       v-btn(text @click='next' color='primary' :loading='loading' :disabled='loading') {{$t('setup.start')}}
         v-icon(v-text='mdiArrowRight')
 </template>
 <script>
-import { mdiArrowRight } from '@mdi/js'
+import { mdiArrowRight, mdiAlert } from '@mdi/js'
 
 export default {
   data () {
     return {
-      mdiArrowRight,
+      mdiArrowRight, mdiAlert,
       loading: false,
       user: {
         email: 'admin',
