9f8f705aa85cee953088f420b39df37d028843e8
lesion <lesion@autistici.org>
2022-05-23 14:42:48 +0200

    minor on tag page

--- pages/tag/_tag.vue
+++ pages/tag/_tag.vue
@@ -1,10 +1,13 @@
 <template>
-  <v-container id='home' class='pa-0 pa-sm-2' fluid>
-    <v-btn text color='primary' to='/'><v-icon v-text='mdiChevronLeft'/> Home</v-btn><h2 class='d-inline'>{{$t('event.tag_description')}} <u>{{tag}}</u></h2>
+  <v-container id='home' fluid>
+    <div class="mt-3 mb-1 ml-1">
+      <v-btn text color='primary' to='/'><v-icon v-text='mdiChevronLeft'/></v-btn>
+    </div>
+
+    <h1 class='d-block text-h3 font-weight-black text-center align-center text-uppercase mt-5 mb-16 mx-auto w-100 text-underline'><u>{{tag}}</u></h1>
 
     <!-- Events -->
-    <!-- .mb-2.mt-1.pl-1.pl-sm-2 -->
-    <div id="events">
+    <div class="mb-2 mt-1 pl-1 pl-sm-2" id="events">
       <Event :event='event' v-for='(event, idx) in events' :lazy='idx>2' :key='event.id'></Event>
     </div>
   </v-container>
@@ -23,7 +26,7 @@ export default {
   async asyncData ({ $axios, params, error }) {
     try {
       const tag = params.tag
-      const events = await $axios.$get(`/events?tags=${params.tag}`)
+      const events = await $axios.$get(`/events?tags=${tag}`)
       return { events, tag }
     } catch (e) {
       error({ statusCode: 400, message: 'Error!' })
