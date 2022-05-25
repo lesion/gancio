b2fe97699803ef698f84da29acf9fde4bf354dba
lesion <lesion@autistici.org>
2022-05-25 10:55:57 +0200

    minor

--- pages/g/_cohort.vue
+++ pages/g/_cohort.vue
@@ -1,16 +1,14 @@
 <template>
-  <v-container id='home' class='pa-0 pa-sm-2' fluid>
-
-   <div class="mt-3 mb-1">
-    <v-btn text color='primary' to='/'><v-icon v-text='mdiChevronLeft'/> Home</v-btn>
-    <v-btn v-for='cohort in cohorts' text color='primary' :key='cohort.id' :to='`/g/${cohort.name}`' v-text='cohort.name' />
-   </div>
+  <v-container id='home' fluid>
+    <div class="mt-3 mb-1 ml-1">
+      <v-btn text color='primary' to='/'><v-icon v-text='mdiChevronLeft'/></v-btn>
+      <v-btn v-for='cohort in cohorts' text color='primary' :key='cohort.id' :to='`/g/${cohort.name}`' v-text='cohort.name' />
+    </div>
 
     <h1 class='d-block text-h3 font-weight-black text-center align-center text-uppercase mt-10 mb-12 mx-auto w-100 text-underline'><u>{{cohort}}</u></h1>
 
     <!-- Events -->
-    <!-- .mb-2.mt-1.pl-1.pl-sm-2 -->
-    <div id="events">
+    <div class='mb-2 mt-1 pl-1 pl-sm-2' id="events">
       <Event :event='event' v-for='(event, idx) in events' :lazy='idx>2' :key='event.id'></Event>
     </div>
   </v-container>
