3dcbd67ab3f0b0808668ee5a382c560eac288e46
lesion <lesion@autistici.org>
2022-06-24 15:57:12 +0200

    remove useless migration

--- server/migrations/20210308194339-sending_notification_status.js
+++ server/migrations/20210308194339-sending_notification_status.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.sequelize.query('ALTER TYPE "enum_event_notifications_status" ADD VALUE \'sending\'').catch(e => {})
+    // return queryInterface.sequelize.query('ALTER TYPE "enum_event_notifications_status" ADD VALUE \'sending\'').catch(e => {})
   },
 
   down: (queryInterface, Sequelize) => {
