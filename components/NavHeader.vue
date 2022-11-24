0d9f2db908b07a7763041d2ff7161f6b282f3020
lesion <lesion@autistici.org>
2022-11-24 17:27:47 +0100

    minor

--- components/NavHeader.vue
+++ components/NavHeader.vue
@@ -1,18 +1,20 @@
 <template>
   <div class='d-flex pa-4'>
-    <img src='/logo.png' height='40' />
+    <v-btn icon large nuxt to='/'>
+      <img src='/logo.png' height='40' />
+    </v-btn>
 
     <v-spacer/>
 
     <div class='d-flex'>
-      <v-btn class='mr-0' icon to='/about' :title='$t("common.about")' :aria-label='$t("common.about")'>
+      <v-btn icon large to='/about' :title='$t("common.about")' :aria-label='$t("common.about")'>
         <v-icon v-text='mdiInformation' />
       </v-btn>
 
       <client-only>
         <v-menu offset-y transition="slide-y-transition">
           <template v-slot:activator="{ on, attrs }">
-            <v-btn icon v-bind='attrs' v-on='on' aria-label='Language' v-text="$i18n.locale" />
+            <v-btn icon large v-bind='attrs' v-on='on' aria-label='Language' v-text="$i18n.locale" />
           </template>
           <v-list>
             <v-list-item v-for='locale in $i18n.locales' @click.prevent.stop="$i18n.setLocale(locale.code)" :key='locale.code'>
@@ -27,13 +29,13 @@
             </v-list-item>
           </v-list>
         </v-menu>
-        <v-btn slot='placeholder' icon arial-label='Language'>{{$i18n.locale}}</v-btn>
+        <v-btn slot='placeholder' large  icon arial-label='Language'>{{$i18n.locale}}</v-btn>
       </client-only> 
 
       <client-only>
         <v-menu v-if='$auth.loggedIn' offset-y transition="slide-y-transition">
           <template v-slot:activator="{ on, attrs }">
-            <v-btn class='mr-0' icon v-bind='attrs' v-on='on' title='Menu' aria-label='Menu'>
+            <v-btn class='mr-0' large icon v-bind='attrs' v-on='on' title='Menu' aria-label='Menu'>
               <v-icon v-text='mdiDotsVertical' />
             </v-btn>
           </template>
@@ -66,14 +68,14 @@
           </v-list>
         </v-menu>
         <template #placeholder>
-          <v-btn v-if='$auth.loggedIn' icon aria-label='Menu' title='Menu'>
+          <v-btn v-if='$auth.loggedIn' large icon aria-label='Menu' title='Menu'>
             <v-icon v-text='mdiDotsVertical' />
           </v-btn>
         </template>
       </client-only>
 
       <!-- login button -->
-      <v-btn class='mr-0' v-if='!$auth.loggedIn' icon nuxt  to='/login' :title='$t("common.login")' :aria-label='$t("common.login")'>
+      <v-btn class='mr-0' v-if='!$auth.loggedIn' large icon nuxt  to='/login' :title='$t("common.login")' :aria-label='$t("common.login")'>
         <v-icon v-text='mdiLogin' />
       </v-btn>
     </div>
