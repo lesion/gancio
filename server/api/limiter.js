13abe1748c8fe26a4b3ba6a5c3c849cdf6d492d4
lesion <lesion@autistici.org>
2023-10-25 09:58:34 +0200

    fly down

--- server/api/limiter.js
+++ server/api/limiter.js
@@ -21,11 +21,11 @@ const instanceApiRateLimiter = {
    * (used during the registration, pass recovery, posting events) */
   SPAMProtectionApiRateLimiter: (process.env.NODE_ENV === 'test' ? next : rateLimit({
     windowMs: 5 * 60 * 1000, // 5 minutes
-    max: 3, // Limit each IP to 3 requests per `window` (here, per 5 minutes)
+    max: 6, // Limit each IP to 3 requests per `window` (here, per 5 minutes)
     standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
     legacyHeaders: false, // Disable the `X-RateLimit-*` headers
     handler: (request, response, next, options) => {
-      log.warn(`SPAM protection api rate limiter: 3req/5min/ip ${request.ip}`)
+      log.warn(`SPAM protection api rate limiter: 6req/5min/ip ${request.ip}`)
       return response.status(options.statusCode).send(options.message)    
     }
   }))
