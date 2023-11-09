58008e8cd6e92b86eb0c7d29e390962ec1262820
lesion <lesion@autistici.org>
2023-11-09 16:57:00 +0100

    migration

---
+++ server/migrations/20231110214302-federation.js
@@ -0,0 +1,40 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    return Promise.all(
+      [
+        await queryInterface.addColumn('ap_users', 'following', { type: Sequelize.BOOLEAN }),
+        await queryInterface.addColumn('events', 'apUserApId', {
+          type: Sequelize.STRING,
+          references: {
+            model: 'ap_users',
+            key: 'ap_id'
+          },
+          onUpdate: 'CASCADE',
+          onDelete: 'CASCADE'
+        }),
+        // apUserApId
+      ])    
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    return Promise.all(
+      [
+        await queryInterface.removeColumn('ap_users', 'following'),
+      ])
+  }
+};
