c95250d9effbda1b68cbff4c9a780d6f01951788
lesion <lesion@autistici.org>
2023-11-09 16:56:46 +0100

    event is coming from fedi

--- server/federation/inbox.js
+++ server/federation/inbox.js
@@ -1,5 +1,6 @@
 const Follows = require('./follows')
 const Resources = require('./resources')
+const Events = require('./events')
 const Ego = require('./ego')
 const log = require('../log')
 
@@ -30,15 +31,25 @@ module.exports = async (req, res) => {
       Ego.bookmark(req, res)
       break
     case 'Delete':
-      await Resources.remove(req, res)
+      if (b.object.type === 'Note') {
+        await Resources.remove(req, res)
+      } else if (b.object.type === 'Event') {
+        await Events.remove(req, res)
+      }
       break
+    case 'Update':
+      if (b.object.type === 'Event') {
+        log.debug(`Event update is coming from ${res.locals.fedi_user}`)
+        await Event.update(req, res)
+      }
     case 'Create':
       // this is a reply
       if (b.object.type === 'Note') {
         log.debug('Create a resource!')
         await Resources.create(req, res)
       } else if (b.object.type === 'Event') {
-        log.debug('Event type is coming!!')
+        log.debug(`Event is coming from ${res.locals.fedi_user}`)
+        await Events.create(req, res)
       } else {
         // await Resources.create(req, res)
         log.warn(`Create with unsupported Object or not a reply => ${b.object.type}`)
