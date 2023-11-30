e72c1e05d2f5b2c402f3ff32941812e27d10ca86
lesion <lesion@autistici.org>
2023-11-30 15:24:20 +0100

    checks for APUser followed/trusted on event create/updated/removed

--- server/federation/events.js
+++ server/federation/events.js
@@ -3,7 +3,7 @@ const helpers = require('../helpers')
 const linkifyHtml = require('linkify-html')
 const dayjs = require('dayjs')
 const eventController = require('../api/controller/event')
-const { Event } = require('../api/models/models')
+const { Event, APUser } = require('../api/models/models')
 const tagController = require('../api/controller/tag')
 
 module.exports = {
@@ -13,22 +13,15 @@ module.exports = {
 
     const APEvent = req.body?.object
 
-
-    // check if we are following this user
-    console.error(req.body)
-
-    //
-    console.error(APEvent)
-
     // check if this event is new
-    // const exists = await Event.findAll({ where: { }})
+    const ap_id = req.body.id
+    const exists = await Event.findOne({ where: { ap_id }})
+    if (exists) { return res.sendStatus(404)}
 
     const place = await eventController._findOrCreatePlace({
       place_name: APEvent.location?.name,
       place_address: APEvent.location?.address,
-
     })
-
 
     let media = []
     if (APEvent.attachment.length > 0) {
@@ -48,9 +41,7 @@ module.exports = {
       }]
     }
 
-
     // create it
-    console.error(Event)
     const event = await Event.create({
       title: APEvent.name.trim(),
       start_datetime: dayjs(APEvent.startTime).unix(),
@@ -58,6 +49,7 @@ module.exports = {
       description: helpers.sanitizeHTML(linkifyHtml(APEvent.content)),
       media,
       is_visible: true,
+      ap_id,
       apUserApId: req.body.actor,
     })
 
@@ -78,11 +70,59 @@ module.exports = {
   async update (req, res) {
     const APEvent = req.body?.object
 
-    // check if we are following this user
-    console.error(req.body)
+    // check if this event is new
+    const ap_id = req.body.id
+    const event = await Event.findOne({ where: { ap_id }})
+    if (!event) { return res.sendStatus(404)}
 
-    //
-    console.error(APEvent)
+    // TODO: is the owner the same?
+
+
+    const place = await eventController._findOrCreatePlace({
+      place_name: APEvent.location?.name,
+      place_address: APEvent.location?.address,
+    })
+
+    let media = []
+    if (APEvent.attachment.length > 0) {
+
+      const image_url = APEvent.attachment[0]?.url
+      req.file = await helpers.getImageFromURL(image_url)
+
+      // let focalpoint = body.image_focalpoint ? body.image_focalpoint.split(',') : ['0', '0']
+      // focalpoint = [parseFloat(parseFloat(focalpoint[0]).toFixed(2)), parseFloat(parseFloat(focalpoint[1]).toFixed(2))]
+      media = [{
+        url: req.file.filename,
+        height: req.file.height,
+        width: req.file.width,
+        name: APEvent.attachment[0]?.name || APEvent.name.trim() || '',
+        size: req.file.size || 0,
+        focalpoint: APEvent.attachment[0]?.focalPoint
+      }]
+    }
+
+    // create it
+    event.update({
+      title: APEvent.name.trim(),
+      start_datetime: dayjs(APEvent.startTime).unix(),
+      end_datetime: dayjs(APEvent.endTime).unix(),
+      description: helpers.sanitizeHTML(linkifyHtml(APEvent.content)),
+      media,
+      is_visible: true,
+      ap_id,
+      apUserApId: req.body.actor,
+    })
+
+    await event.setPlace(place)
+
+    // create/assign tags
+    let tags = []
+    if (APEvent.tag) {
+      tags = await tagController._findOrCreate(APEvent.tag.map(t => t?.name.substr(1)))
+      await event.setTags(tags)
+    }
+
+    return event
 
   },
 
