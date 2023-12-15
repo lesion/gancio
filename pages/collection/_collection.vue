185b8bd75e3cf9dddb160100d601951b60ab379e
lesion <lesion@autistici.org>
2023-12-15 17:20:10 +0100

    fix: missing some style in detail collection page

--- pages/collection/_collection.vue
+++ pages/collection/_collection.vue
@@ -1,11 +1,11 @@
 <template>
-  <v-container class='px-0' fluid>
+  <v-container id='home' class='px-2 px-sm-6 pt-0' fluid>
 
-    <h1 class='d-block text-h3 font-weight-black text-center align-center text-uppercase mt-10 mb-12 mx-auto w-100 text-underline'><u>{{collection}}</u></h1>
+    <h1 class='d-block text-h3 font-weight-black text-center text-uppercase mt-10 mb-16 mx-auto w-100 text-underline'><u>{{collection}}</u></h1>
 
     <!-- Events -->
-    <div class='mb-2 mt-1 pl-1 pl-sm-2' id="events">
-      <v-lazy class='event' :value='idx<9' v-for='(event, idx) in events' :key='event.id' :min-height='hide_thumbs ? 105 : undefined' :options="{ threshold: .5, rootMargin: '500px' }">
+    <div class="mb-2 mt-1 pl-1 pl-sm-2" id="events">
+      <v-lazy class='event v-card' :value='idx<9' v-for='(event, idx) in events' :key='event.id' :min-height='hide_thumbs ? 105 : undefined' :options="{ threshold: .5, rootMargin: '500px' }" :class="{ 'theme--dark': is_dark }">
         <Event :event='event' :lazy='idx>9' />
       </v-lazy>
     </div>
@@ -13,7 +13,7 @@
 </template>
 <script>
 
-import { mapState } from 'vuex'
+import { mapState, mapGetters } from 'vuex'
 import Event from '@/components/Event'
 
 export default {
@@ -29,7 +29,10 @@ export default {
       ]
     }
   },
-  computed: mapState(['settings']),  
+  computed: {
+    ...mapState(['settings']),
+    ...mapGetters(['hide_thumbs', 'is_dark']),
+  },
   async asyncData ({ $axios, params, error }) {
     try {
       const collection = params.collection
