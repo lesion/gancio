841e2b69c06125c72adc4eeb1b40316787f1cc3a
sedum <sedum@oziosi.org>
2024-01-17 19:16:14 +0100

    fix: ui color theme wrong selection #332

--- vuetify.options.js
+++ vuetify.options.js
@@ -4,7 +4,10 @@ import { ca, cs, de, en, es, eu, fr, gl, it, nb, nl, pl, pt, tr, sk, ro, ru, zhH
 export default ({ res, nuxtState }) => {
 
   const settings = process.server ? (res.locals.settings || {}) : nuxtState.state.settings || {}
-  
+  const is_dark = process.client
+    ? (typeof nuxtState.state.localSettings['theme.is_dark'] !== 'undefined' ? nuxtState.state.localSettings['theme.is_dark'] : settings['theme.is_dark'])
+    : settings['theme.is_dark']
+
   return {
     lang: { locales: { ca, cs, de, en, es, eu, fr, gl, it, nb, nl, pl, pt, sk, tr, ro, ru, zhHans } },
     treeShake: true,
@@ -14,7 +17,7 @@ export default ({ res, nuxtState }) => {
         variations: false,
         minifyTheme,
       },
-      dark: settings['theme.is_dark'],
+      dark: is_dark,
       themes: {
         dark: settings.dark_colors,
         light: settings.light_colors
