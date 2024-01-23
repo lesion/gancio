2cc4da992dfb5cba5495e6938556ff38dd826e7c
lesion <lesion@autistici.org>
2024-01-23 09:15:01 +0100

    minor

--- pages/about.vue
+++ pages/about.vue
@@ -1,13 +1,12 @@
 <template lang="pug">
 v-container
-  v-card
-    v-card-text(v-if='$auth.user && $auth.user.is_admin')
-      Editor.px-3.ma-0(v-model='about' :label="$t('common.about')")
-    v-card-text(v-else v-html='about')
-    v-card-actions(v-if='$auth.user && $auth.user.is_admin')
-      v-spacer
-      v-btn(color='primary' outlined
-        @click='save') {{$t('common.save')}}
+  v-card-text(v-if='$auth.user && $auth.user.is_admin')
+    Editor.px-3.ma-0(v-model='about' :label="$t('common.about')")
+  v-card-text(v-else v-html='about')
+  v-card-actions(v-if='$auth.user && $auth.user.is_admin')
+    v-spacer
+    v-btn(color='primary' outlined
+      @click='save') {{$t('common.save')}}
 </template>
 <script>
 import Editor from '@/components/Editor'
