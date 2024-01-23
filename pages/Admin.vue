94a50c39eb9fdff0f161b80cb2afabd151681c30
lesion <lesion@autistici.org>
2024-01-23 09:44:06 +0100

    minor: flat style

--- pages/Admin.vue
+++ pages/Admin.vue
@@ -1,9 +1,9 @@
 <template lang="pug">
 v-container.container.pa-0.pa-md-3
   v-card
-    v-alert(v-if='url!==settings.baseurl' outlined type='warning' show-icon :icon='mdiAlert')
+    v-alert(v-if='url!==settings.baseurl' rounded="0" flat border="left" type='warning' show-icon :icon='mdiAlert')
       span(v-html="$t('admin.wrong_domain_warning', { url, baseurl: settings.baseurl })")
-    v-alert(v-if='!selfReachable' outlined type='warning' show-icon :icon='mdiAlert')
+    v-alert(v-if='!selfReachable' rounded="0" flat border="left" type='warning' show-icon :icon='mdiAlert')
       span(v-html="$t('admin.not_reachable_warning', { baseurl: settings.baseurl })")
     v-tabs(v-model='selectedTab' show-arrows :next-icon='mdiChevronRight' :prev-icon='mdiChevronLeft')
 
