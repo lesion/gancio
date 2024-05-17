ac592ac0019adc9a591fb13e74e5f4a72f28a2bc
lesion <lesion@autistici.org>
2024-05-17 08:51:54 +0200

    minor on announcement component

--- components/Announcement.vue
+++ components/Announcement.vue
@@ -1,10 +1,15 @@
 <template>
   <nuxt-link :to='`/announcement/${announcement.id}`'>
-    <v-alert class='mb-1 text-h5 font-weight-bold' rounded="0" type='info' color="primary" flat border="left">{{announcement.title}}</v-alert>
+    <v-alert rounded="0" flat  color="primary" border="left" show-icon :icon='mdiInformation'><h3>{{announcement.title}}</h3></v-alert>
   </nuxt-link>
 </template>
 <script>
+import { mdiInformation } from '@mdi/js'
+
 export default {
+  data () {
+    return { mdiInformation }
+  },
   props: {
     announcement: { type: Object, default: () => ({}) }
   }
